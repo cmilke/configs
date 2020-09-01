7e7a2f33377483feda4908d421ae780797076849
Chris Milke <chrisdmilke@gmail.com>
2020-09-01 09:11:38 -0500

    moving all configs to separate directory

---
+++ set_configs.sh
@@ -0,0 +1,14 @@
+echo ENSURE YOU RUN THIS FROM WITHIN THE CONFIG DIRECTORY!
+echo EXECUTING IN 5; sleep 1
+echo 4; sleep 1;
+echo 3; sleep 1;
+echo 2; sleep 1;
+echo 1; sleep 1;
+config_dir=$PWD
+cd $HOME
+rm -r .vim  .vimrc &> /dev/null
+files_to_link='.allshrc  .tmux.conf  .vim  .vimrc'
+for f in $files_to_link; do
+    ln -s ${config_dir}/$f .
+done
+echo CONFIGS LINKED
